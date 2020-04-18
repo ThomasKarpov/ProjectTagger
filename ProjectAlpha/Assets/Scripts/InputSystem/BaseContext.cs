794687a3942533493ed6af0073a042f3c40cb195
rzu-7 <dev.rzu7@gmail.com>
2020-04-18 11:19:31 -0700

    Implemented the context driven input system.
    The input system monitors for user input and triggers the corresponding events. The action taken based on the
    event depends on the current context being used. The contexts are dynamically swappable. This allows for
    different uses for the same user input depending on the context. i.e. gameplay vs menu naviation.
    
    Demo Scene is included in Assets/Scenes/InputSystem/InputSystemDemo.unity

---
+++ ProjectAlpha/Assets/Scripts/InputSystem/BaseContext.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Project Name: Project Alpha
+ *       Author: RZU-7
+ *         Date: 2020/04/18
+ *  Description: The base class for the input contexts.
+ */
+
+using UnityEngine;
+
+public abstract class BaseContext : MonoBehaviour
+{
+    /*
+     * @brief Abstract method for executing an action based on the leftButton press.
+     *
+     * @param value The raw axis value of the axis input
+     */
+    abstract public void LeftButtonAction(float value);
+
+    /*
+     * @brief Abstract method for executing an action based on the rightButton press.
+     * 
+     * @param value The raw axis value of the axis input
+     */
+    abstract public void RightButtonAction(float value);
+
+    /*
+     * @brief Abstract method for executing an action based on the upButton press.
+     *
+     * @param value The raw axis value of the axis input
+     */
+    abstract public void UpButtonAction(float value);
+
+    /*
+     * @brief Abstract method for executing an action based on the downButton press.
+     *
+     * @param value The raw axis value of the axis input
+     */
+    abstract public void DownButtonAction(float value);
+
+    /*
+     * @brief Abstract method for executing an action based on the action1Button press.
+     */
+    abstract public void Action1ButtonAction();
+
+    /*
+     * @brief Abstract method for executing an action based on the action2Button press.
+     */
+    abstract public void Action2ButtonAction();
+
+    /*
+     * @brief Abstract method for executing an action based on the optionsButton press.
+     */
+    abstract public void OptionsButtonAction();
+}
---
+++ ProjectAlpha/Assets/Scripts/InputSystem/InputMonitor.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * Project Name: Project Alpha
+ *       Author: RZU-7
+ *         Date: 2020/04/18
+ *  Description: Monitors the player input and triggers events if player input
+ *               is detected. The input names MUST be defined in the Unity
+ *               Input Manager.
+ */
+
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InputMonitor : MonoBehaviour
+{
+    public FloatEvent leftButton;
+    public FloatEvent rightButton;
+    public FloatEvent upButton;
+    public FloatEvent downButton;
+    public UnityEvent action1Button;
+    public UnityEvent action2Button;
+    public UnityEvent optionsButton;
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckAxis(Input.GetAxisRaw("Horizontal"), leftButton, rightButton);
+
+        CheckAxis(Input.GetAxisRaw("Vertical"), downButton, upButton);
+
+        CheckInput(Input.GetButtonDown("Action1"), action1Button);
+
+        CheckInput(Input.GetButtonDown("Action2"), action2Button);
+
+        CheckInput(Input.GetButtonDown("Options"), optionsButton);
+    }
+
+    /*
+     * @brief Check the value of an axis and trigger the corresponding event.
+     *
+     * @param axisValue The raw axis value of the axis input
+     *
+     * @param negativeEvent The FloatEvent to trigger if the raw value is negative
+     *
+     * @param positiveEvent The FloatEvent to trigger if the raw value is positive
+     */
+    void CheckAxis(float axisValue, FloatEvent negativeEvent, FloatEvent positiveEvent)
+    {
+        if (Mathf.Approximately(0, axisValue))
+        {
+            return;
+        }
+
+        if (axisValue < 0)
+        {
+            negativeEvent.Invoke(axisValue);
+            return;
+        }
+
+        if (axisValue > 0)
+        {
+            positiveEvent.Invoke(axisValue);
+            return;
+        }
+    }
+
+    /*
+     * @brief Checks if a button is pressed trigger the corresponding event.
+     *
+     * @param buttonPress The boolean status of the button press
+     *
+     * @param buttonEvent The UnityEvent to trigger if the button is pressed
+     */
+    void CheckInput(bool buttonPress, UnityEvent buttonEvent)
+    {
+        if (buttonPress)
+        {
+            buttonEvent.Invoke();
+        }
+    }
+}
