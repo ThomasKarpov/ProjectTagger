ac1f1b5f46baf102e7dd5b8d0e1717cbca72090e
Amuria <amuria@outlook.de>
2020-05-06 17:34:48 +0200

    Score&Time Managment Initial
    Folder for Score/Time managment
    Timer script
    Score Manager script
    UI script for Score and Time
    Prefab of UI combined with managers

---
+++ ProjectAlpha/Assets/Scripts/ScoreManagment/ScoreManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField]
+    Timer _timer = null;
+    [SerializeField]
+    private UIScoreManager _uiScoreManager = null;
+    private int _catcherScore = 0;
+    private int _runnerScore = 0;
+    void Start()
+    {
+        Timer.OnTimeSpent += _OutOfTime;
+        //CatchMechanic.OnCaught+=_CaughtRunner; //turn me on when ready
+        _timer.StartTimer();
+    }
+    private void _OutOfTime() 
+    {
+        _runnerScore++;
+        _uiScoreManager.ShowVictory("Runner", false, _catcherScore, _runnerScore);
+        
+    }
+    private void _CaughtRunner() 
+    {
+        _catcherScore++;
+        _uiScoreManager.ShowVictory("Catcher", true, _catcherScore, _runnerScore);
+        _timer.StopTimer();
+
+    }
+}
---
+++ ProjectAlpha/Assets/Scripts/ScoreManagment/UIScoreManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScoreManager : MonoBehaviour
+{
+    
+    [SerializeField]
+    private Text _winner=null;
+    [SerializeField]
+    private Text _winCondition = null;
+    [SerializeField]
+    private Text timerDisplayed = null;
+    [SerializeField]
+    private Text _timeRemaining = null;
+    [SerializeField]
+    private Text _catcherScore = null;
+    [SerializeField]
+    private Text _runnerScore = null;
+    [SerializeField]
+    GameObject winUI = null;
+    public void ShowVictory(string winner, bool caught, int catcherScore, int runnerScore) 
+    {
+        _UpdateWinner(winner, caught);
+        _timeRemaining.text = timerDisplayed.text;
+        _UpdateScores(catcherScore, runnerScore);
+        winUI.SetActive(true);
+    }
+    private void _UpdateWinner(string winner, bool caught) 
+    {
+        _winner.text = winner;
+        if (caught)
+            _winCondition.text = "by catching everyone";
+        else
+            _winCondition.text = "by evading being caught";
+    }
+    private void _UpdateScores(int catcherScore, int runnerScore) 
+    {
+        _catcherScore.text = catcherScore.ToString();
+        _runnerScore.text = runnerScore.ToString();
+    }
+}
